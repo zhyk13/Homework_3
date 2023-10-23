a664c4e5ab7c6be84327a700dcbcce38d0d8a3d2
zhyk <zhyk@list.ru>
2023-10-23 16:40:05 +0300

    Третья задача

---
+++ Task03/Program.cs
@@ -0,0 +1,7 @@
+﻿// Принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+}
