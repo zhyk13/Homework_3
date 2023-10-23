b37ceac48e57c84fd820c3d9e7317941553af72d
zhyk <zhyk@list.ru>
2023-10-23 10:45:00 +0300

    Первая задача

---
+++ Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Принимает на вхд пятизначное число и проверяет,
+// является ли оно палиндромом.
+Console.Write("Введите любое целое число: ");
+string? number = Console.ReadLine();
+int length = number.Length;
+int count = 0;
+while ((count <= (length/2 -1)) && (number[count] == number[length - 1 - count]))
+{
+    count++;
+}
+if (count == length/2)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
